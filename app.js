ab45c95922b354ea0b70038a8c4607dd6516be8c
이민철 <lmc@imincheol-ui-MacBookPro.local>
2022-04-20 23:42:24 +0900

    202204202342

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const loginForm  = document.querySelector("#login-form"); 
 
-
-function onLoginBtnClick() {
+function onLoginSubmit(event) {
+    event.preventDefault();
     // console.log(loginInput.value);
-    const username = loginInput.value;
-    console.log(username);
+    // const username = loginInput.value;
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+// functiondp () 즉시실행s

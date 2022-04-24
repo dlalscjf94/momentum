360c9cf00b12b1d795af6df6b5b37eba8ba67951
이민철 <lmc@imincheol-ui-MacBookPro.local>
2022-04-24 15:47:15 +0900

    20220424

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm  = document.querySelector("#login-form"); 
+const greeting = document.querySelector("#greet");
+
+const CLASS_HIDDEN = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     // console.log(loginInput.value);
     // const username = loginInput.value;
     console.log(loginInput.value);
+    const username = loginInput.value;
+    loginForm.classList.add(CLASS_HIDDEN);
+    console.log(username);
+    // greeting.innerText = "hello " + username;
+    greeting.innerText = 'hello ${username}';
+    greeting.classList.remove(CLASS_HIDDEN);
+    
 }
 
 

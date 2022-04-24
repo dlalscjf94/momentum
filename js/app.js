cee1d9095f1618bdcd7dc506eba0b48a1c4b55d1
이민철 <lmc@imincheol-ui-MacBookPro.local>
2022-04-24 17:35:32 +0900

    padStart

--- js/app.js
+++ js/app.js
@@ -29,7 +29,11 @@ function paintGreeting(username){
 
 function getClock(){
     const date = new Date();
-    clock.innerText = `${date.getHours()}: ${date.getMinutes()}: ${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+
+    clock.innerText = `${hours}: ${minutes}: ${seconds}`;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);

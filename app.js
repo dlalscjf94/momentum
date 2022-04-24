08202e1624744c0939f2f950e6d3b99dfac71f0c
이민철 <lmc@imincheol-ui-MacBookPro.local>
2022-04-24 16:23:24 +0900

    localstorage

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const loginForm  = document.querySelector("#login-form");
 const greeting = document.querySelector("#greet");
 
 const CLASS_HIDDEN = "hidden";
+const USERNAME_KEY = "username";
+
 
 function onLoginSubmit(event) {
     event.preventDefault();
@@ -13,12 +15,24 @@ function onLoginSubmit(event) {
     loginForm.classList.add(CLASS_HIDDEN);
     console.log(username);
     // greeting.innerText = "hello " + username;
-    greeting.innerText = 'hello ${username}';
+    // backtic
+    greeting.innerText = `hello ${username}`;
     greeting.classList.remove(CLASS_HIDDEN);
+    localStorage.setItem(USERNAME_KEY, username);
     
 }
 
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedusername = localStorage.getItem(USERNAME_KEY);
+
+
+if (savedusername == "") {
+    loginForm.classList.remove("hidden");
+} 
+else {
+    greet.classList.remove("hidden");
+}
 
 // functiondp () 즉시실행s

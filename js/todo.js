731b1be3a1e2944a7e2c7e461c8243cdd406af5a
이민철 <lmc@imincheol-ui-MacBookPro.local>
2022-05-02 23:34:59 +0900

    delete Todos localStorage, id

--- js/todo.js
+++ js/todo.js
@@ -3,7 +3,7 @@ const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
-const toDos = []; 
+let toDos = []; 
 
 function saveToDos(){ 
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
@@ -12,14 +12,17 @@ function saveToDos(){
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();   
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 
 function paintTodo(newToDo){
     console.log("i will paint", newToDo);
     const li = document.createElement("li");
+    li.id = newToDo.id;
     const span = document.createElement("span");
-    span.innerText = newToDo;
+    span.innerText = newToDo.text;
 
     const delete_btn = document .createElement("button");
     delete_btn.innerText = "delete";
@@ -36,9 +39,14 @@ function handleTodoSubmit(event){
     event.preventDefault();
     const newToDo  = toDoInput.value;
     toDoInput.Value = "";
-    console.log(newToDo, toDoInput.value);
-    toDos.push(newToDo);
-    paintTodo(newToDo);
+    const newTodoObj = {
+        text: newToDo, 
+        id: Date.now(),
+    };
+    // console.log(newToDo, toDoInput.value);
+    // toDos.push(newToDo);
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveToDos();
 }
 
@@ -49,5 +57,14 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 console.log(savedToDos);
 if (savedToDos !== null) {
     const parsedToDos = JSON.parse(savedToDos);
-    parsedToDos.forEach((item) => console.log("this is turn of", item));
+    // parsedToDos.forEach((item) => console.log("this is turn of", item));
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintTodo);
 }
+
+
+// ex filter
+const arr = ["pizza", "banana", "potato"];
+function sexyfilter(food){return food !== "banana"};
+arr.filter(sexyfilter);
+// result => ["pizza", "potato"]
